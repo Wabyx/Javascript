5ab54f84f78a647d1a9bce0e6427343e23f4dca7
Yoonbin Kim <88513592+Wabyx@users.noreply.github.com>
2022-06-06 19:23:40 +0900

    Add files via upload

---
+++ practice.js
@@ -0,0 +1,64 @@
+// document.getElementById("lion").style.color = "red";
+// document.getElementById("tiger").style.color = "blue";
+// document.getElementById("bear").style.color = "green";
+
+// const animal = document.getElementsByTagName("li");
+// document.getElementsByTagName("li")[0].style.color = "red";
+
+// document.getElementsByClassName("animal")[1].style.color = "red";
+// document.querySelectorAll(".animal")[2].innerHTML = "dog";
+// const animals = document.getElementById("animals");
+
+// animals.innerHTML += "<li class = 'animal'> Cat </li>";
+
+// document.querySelectorAll(".box")[0].classList.add("purple");
+
+// document.querySelectorAll(".box")[0].classList.remove("purple");
+
+// document.querySelectorAll(".box")[0].classList.toggle("yellow");
+// document.querySelectorAll(".box")[0].classList.toggle("yellow");
+
+// document.getElementById("btn").addEventListener("click", function () {
+//     console.log("click!!!!");
+// });
+
+// var num = 0;
+// document.getElementById("plus").addEventListener("click", function () {
+//     num++;
+//     document.getElementById("num").innerHTML = num;
+// });
+
+// document.getElementById("minus").addEventListener("click", function () {
+//     num--;
+//     document.getElementById("num").innerHTML = num;
+// }); 
+
+// document.querySelector(".bar").addEventListener("click", function () {
+//     document.querySelector(".bar").innerHTML = "눌렀어!";
+//     document.querySelector(".newBar").classList.toggle("show");
+// });
+
+document.querySelector(".list")[0].addEventListener("click", function (e) {
+    tabBtn(e.target.dataset.id);
+});
+
+function tabBtn(a) {
+    document
+    .querySelectorAll(".tab-button")[a]
+    .addEventListener("click", function () {
+        for (let i = 0; 
+            i < document.querySelectorAll(".tab-button").length; 
+            i++) {
+            document.querySelectorAll(".tab-button")[i].classList.remove("here");
+            document.querySelectorAll(".tab-content")[i].classList.remove("show"); 
+        }
+    
+        document.querySelectorAll(".tab-button")[a].classList.add("here");
+        document.querySelectorAll(".tab-content")[a].classList.add("show"); 
+    });
+}
+
+
+
+
+
